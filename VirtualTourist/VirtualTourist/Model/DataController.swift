6ce3b8b322b7b711cef82f078972cc34637d808d
Abhilash Khare <abhilashkhare@gmail.com>
2018-03-30 01:24:32 -0700

    first commit

---
+++ VirtualTourist/VirtualTourist/Model/DataController.swift
@@ -0,0 +1,31 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Abhilash Khare on 3/30/18.
+//  Copyright © 2018 Abhilash Khare. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer : NSPersistentContainer
+    
+    var viewContext : NSManagedObjectContext{
+        return  persistentContainer.viewContext
+    }
+    
+    init(modelName : String){
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+}
---
+++ VirtualTourist/VirtualTourist/ViewController/VirtualTouristViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Abhilash Khare on 3/29/18.
+//  Copyright © 2018 Abhilash Khare. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class VirtualTouristViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var deletePinButton: UIButton!
+    
+    var dataController : DataController!
+    
+    var longPressGestureRecognizer : UILongPressGestureRecognizer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       deletePinButton.isHidden = true
+       deletePinButton.isEnabled = false
+    
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self,action: #selector(addAnnotations(longPressgestureRecongizer :)))
+        longPressGestureRecognizer.minimumPressDuration = 2.0
+        mapView.addGestureRecognizer(longPressGestureRecognizer)
+        
+        
+        
+    }
+
+
+    @IBAction func pressEdit(_ sender: Any) {
+        if(editButton.title == "Edit"){
+        deletePinButton.isHidden = false
+        deletePinButton.isEnabled = true
+        editButton.title = "Done"
+    }
+    else{
+    deletePinButton.isHidden = true
+    deletePinButton.isEnabled = false
+    editButton.title = "Edit"
+    }
+    }
+    @IBAction func deletePinsMethod(_ sender: Any) {
+     
+    }
+    
+    @objc func addAnnotations(longPressgestureRecongizer : UILongPressGestureRecognizer){
+        
+        if longPressgestureRecongizer.state == UIGestureRecognizerState.began {
+            var touchPoint = longPressgestureRecongizer.location(in: mapView)
+            var newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+            
+            let pin = Pin(context: self.dataController.viewContext)
+            pin.lat = annotation.coordinate.latitude
+            pin.lon = annotation.coordinate.longitude
+            try? dataController.viewContext.save()
+            
+        }
+        
+    }
+    
+    
+}
+
