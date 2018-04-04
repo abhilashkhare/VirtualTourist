7f8c744c6d4c205aa009b9a0f87257d379ee7855
Abhilash Khare <abhilashkhare@gmail.com>
2018-04-03 21:23:27 -0700

    fifth commit

--- VirtualTourist/VirtualTourist/Controller/VirtualTouristViewController.swift
+++ VirtualTourist/VirtualTourist/Controller/VirtualTouristViewController.swift
@@ -14,19 +14,20 @@ class VirtualTouristViewController: UIViewController,MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var editButton: UIBarButtonItem!
-    @IBOutlet weak var deletePinButton: UIButton!
+    @IBOutlet weak var deletePinsLabel: UILabel!
     
     var dataController : DataController!
     var pins : [Pin] = []
     var annotations = [MKPointAnnotation]()
     let fetchRequest : NSFetchRequest<Pin> = Pin.fetchRequest()
+    var toggleDelete : Bool = false
     
     var longPressGestureRecognizer : UILongPressGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-       deletePinButton.isHidden = true
-       deletePinButton.isEnabled = false
+       deletePinsLabel.isHidden = true
+       deletePinsLabel.isEnabled = false
     
         longPressGestureRecognizer = UILongPressGestureRecognizer(target: self,action: #selector(addAnnotations(longPressgestureRecongizer :)))
         longPressGestureRecognizer.minimumPressDuration = 2.0
@@ -57,19 +58,23 @@ class VirtualTouristViewController: UIViewController,MKMapViewDelegate {
 
     @IBAction func pressEdit(_ sender: Any) {
         if(editButton.title == "Edit"){
-        deletePinButton.isHidden = false
-        deletePinButton.isEnabled = true
+        deletePinsLabel.isHidden = false
+        deletePinsLabel.isEnabled = true
         editButton.title = "Done"
+        toggleDelete = true
+        
+            
     }
     else{
-    deletePinButton.isHidden = true
-    deletePinButton.isEnabled = false
+    deletePinsLabel.isHidden = true
+    deletePinsLabel.isEnabled = false
     editButton.title = "Edit"
+    toggleDelete = false
     }
     }
-    @IBAction func deletePinsMethod(_ sender: Any) {
-     
-    }
+    
+
+ 
     
     @objc func addAnnotations(longPressgestureRecongizer : UILongPressGestureRecognizer){
         
@@ -113,30 +118,47 @@ class VirtualTouristViewController: UIViewController,MKMapViewDelegate {
 
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         print("didselect")
+        print("toggledelete  \(toggleDelete)")
 //            selectPin(selectedPin as! Pin)
+        if(toggleDelete == false){
             let viewController = self.storyboard?.instantiateViewController(withIdentifier: "PhotoAlbumViewController") as! PhotoAlbumViewController
-        if let lat = view.annotation?.coordinate.latitude  {
-            viewController.pin?.lat = lat
-        }
-        if let lon = view.annotation?.coordinate.longitude {
-            viewController.pin?.lon = lon
-        }
+            if let lat = view.annotation?.coordinate.latitude  {
+                viewController.pin?.lat = lat
+            }
+            if let lon = view.annotation?.coordinate.longitude {
+                viewController.pin?.lon = lon
+            }
             viewController.dataController = self.dataController
             self.navigationController?.pushViewController(viewController, animated: true)
-        
+            
         }
-    
+        
+        else if (toggleDelete == true){
+            if let lat = view.annotation?.coordinate.latitude, let lon = view.annotation?.coordinate.longitude{
+                if let result = try? dataController.viewContext.fetch(fetchRequest){
+                    for i in result {
+                        if((i.lat == lat) && (i.lon == lon)){
+                            dataController.viewContext.delete(i)
+                            try? dataController.viewContext.save()
+                            let annotation = MKPointAnnotation()
+                            annotation.coordinate.latitude = lat
+                            annotation.coordinate.longitude = lon
+                            print("annotation.coordinate.latitude \(annotation.coordinate.latitude)")
+                            print("lat \(lat)")
+                            print("annotation.coordinate.longitude \(annotation.coordinate.longitude)")
+                            print("lon \(lon)")
+
+                            mapView.removeAnnotation(annotation)
+                        }
+                    }
+            }
+         }
+        }
+    }
 
     
-//    func selectPin(_ selectedPin : Pin){
-//        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "PhotoAlbumViewController") as! PhotoAlbumViewController
-//        viewController.pin = selectedPin
-//        viewController.dataController = self.dataController
-//        self.navigationController?.pushViewController(viewController, animated: true)
-//    }
 
 
- 
     
 }
 
